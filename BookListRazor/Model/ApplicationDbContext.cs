e054d17cf54808a003c05c4fd0695643bad1622e
Valentindg <valentin.f.dogaru@gmail.com>
2021-02-08 20:06:45 +0200

    Update

---
+++ BookListRazor/Model/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookListRazor.Model
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Book> Book { get; set; }
+    }
+}
